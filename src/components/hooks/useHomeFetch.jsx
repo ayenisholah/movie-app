039809dc61f2900130142ebc5c09a51ff65766c1
Shola Ayeni <ayenisholah@yahoo.com>
2019-12-30 17:02:30 +0100

    created custom hooks to fetch data from the API

---
+++ src/components/hooks/useHomeFetch.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { API_KEY, API_URL } from "../../config";
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState({ movies: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  
+  const fetchMovies = async endpoint => {
+    setError(false);
+    setLoading(true);
+
+    try {
+      const result = await (await fetch(endpoint)).json();
+      setState(prevState => ({
+        ...prevState,
+        movies: [...result.results],
+        heroImage: prevState.heroImage || result.results[0],
+        currentPage: result.page,
+        totalPages: result.total_pages
+      }));
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+  }, []);
+
+  return [{ state, loading, error }, fetchMovies];
+};
