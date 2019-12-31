fc36bc5ac40755a7f2962f9537921ac98a6a2636
Shola Ayeni <ayenisholah@yahoo.com>
2019-12-31 09:21:10 +0100

    saved the api state to session storage

--- src/components/hooks/useHomeFetch.jsx
+++ src/components/hooks/useHomeFetch.jsx
@@ -1,20 +1,26 @@
+/* eslint-disable eqeqeq */
 import { useState, useEffect } from "react";
 import { API_KEY, API_URL } from "../../config";
 
-export const useHomeFetch = () => {
+export const useHomeFetch = searchTerm => {
   const [state, setState] = useState({ movies: [] });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  
+
   const fetchMovies = async endpoint => {
     setError(false);
     setLoading(true);
+
+    const isLoadMore = endpoint.search("page");
 
     try {
       const result = await (await fetch(endpoint)).json();
       setState(prevState => ({
         ...prevState,
-        movies: [...result.results],
+        movies:
+          isLoadMore !== -1
+            ? [...prevState.movies, ...result.results]
+            : [...result.results],
         heroImage: prevState.heroImage || result.results[0],
         currentPage: result.page,
         totalPages: result.total_pages
@@ -27,9 +33,23 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
+  // Fetch popular movies initially on mount
   useEffect(() => {
-    fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+    if (sessionStorage.homeState) {
+      console.log("Grabbing from session storage");
+      setState(JSON.parse(sessionStorage.homeState));
+    } else {
+      console.log("Grabbing from API");
+      fetchMovies(`${API_URL}movie/popular?api_key=${API_KEY}`);
+    }
   }, []);
+
+  useEffect(() => {
+    if (!searchTerm) {
+      console.log("Writing to session storage");
+      sessionStorage.setItem("homeState", JSON.stringify(state));
+    }
+  }, [searchTerm, state]);
 
   return [{ state, loading, error }, fetchMovies];
 };
